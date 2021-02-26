1d0550833c3aec14a84498d0a7df64e468f9ea6b
FrontAlbert <alberthui@outlook.com>
2021-02-25 23:57:25 -0500

    movies

--- main.js
+++ main.js
@@ -1,30 +1,32 @@
-const api_key = 'c2d2d9cda178ce20abd7ab8e83b201bf'
+const api_key = "c2d2d9cda178ce20abd7ab8e83b201bf";
+const people = document.querySelector(".popular-people");
+const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
 async function fetchPeople() {
     const dataFetch = await fetch(
-        `https://api.themoviedb.org/3/trending/person/week?api_key=c2d2d9cda178ce20abd7ab8e83b201bf
+        `https://api.themoviedb.org/3/person/popular?api_key=c2d2d9cda178ce20abd7ab8e83b201bf&language=en-US&page=1
         `
     );
     const data = await dataFetch.json();
-    console.log(data);
+    // console.log(data);
     return data;
 }
 
-const displayPeople = () =>{
-    fetchPeople().then((res)=>{
-        console.log(res.results)
-        array1= res.results
-        array1.slice(0,6).forEach(x =>{
-            
-        })
-        console.log(array1.slice(0,6))
-    })
+const displayPeople = () => {
+    fetchPeople().then((res) => {
+        array1 = res.results;
+        array1.slice(0, 10).forEach((x) => {
+            console.log(x);
+            const markup = `
+            <div class="container">
+                <img class="person" src=${IMAGE_URL + x.profile_path}>
+            </div>
+            `;
+            people.insertAdjacentHTML("beforeend", markup);
+        });
+    });
+};
 
-
-
-}
-
-
-window.onload = (e) =>{
+window.onload = (e) => {
     displayPeople();
-}
+};
