952d02e3db0f9f3a688687b98c2b08f93509f82c
FrontAlbert <alberthui@outlook.com>
2021-02-25 19:30:19 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const api_key = 'c2d2d9cda178ce20abd7ab8e83b201bf'
+
+async function fetchPeople() {
+    const dataFetch = await fetch(
+        `https://api.themoviedb.org/3/trending/person/week?api_key=c2d2d9cda178ce20abd7ab8e83b201bf
+        `
+    );
+    const data = await dataFetch.json();
+    console.log(data);
+    return data;
+}
+
+const displayPeople = () =>{
+    fetchPeople().then((res)=>{
+        console.log(res.results)
+        array1= res.results
+        array1.slice(0,5).forEach(x =>{
+            
+        })
+        console.log(array1.slice(0,5))
+    })
+
+
+
+}
+
+
+window.onload = (e) =>{
+    displayPeople();
+}
