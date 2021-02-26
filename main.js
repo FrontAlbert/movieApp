7e7af53ca02ab01d57092d4e897e5e0a25879a33
FrontAlbert <alberthui@outlook.com>
2021-02-26 15:17:11 -0500

    fix

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ const trending = document.querySelector(".trending-movies");
 const nowPlaying = document.querySelector(".now-playing");
 const IMAGE_URL = "https://image.tmdb.org/t/p/w500";
 
-
-
+// FETCH NOW PLAYING
 async function fetchNowPlaying() {
     const dataFetch = await fetch(
         `https://api.themoviedb.org/3/movie/now_playing?api_key=c2d2d9cda178ce20abd7ab8e83b201bf&language=en-US&page=1
@@ -46,6 +45,44 @@ const displayNowPlaying = () => {
         });
     });
 };
+// FETCH NOW PLAYING
+
+// FETCH TRENDING
+
+const displayTrending = () => {
+    fetchTrending().then((res) => {
+        array1 = res.results;
+        array1.slice(0, 5).forEach((x) => {
+            // console.log(x);
+            const markup = `
+            <div class="markup-container">
+                <div class="overlay-container">
+                    <img class="markup-image" src=${IMAGE_URL + x.poster_path}>
+                    <p class="overlay">${x.overview}</p>
+                </div>
+                <div class="markup-details">
+                    <div class="detail-header">
+                        <h5>${(name = x.original_title
+                            ? `${x.original_title}`
+                            : `${x.original_name}`)}<h5>
+                        <i class="fas fa-crown"> ${x.vote_average}</i>
+                    </div>
+                    <div class="detail-header2">
+                            <h5>${(name = x.release_date ? `${x.release_date}`
+                            : `${x.first_air_date}`)}</h5>
+                            <h5 class="trailer">Trailer<h5>
+                    </div>
+                </div>
+            </div>
+            `;
+            trending.insertAdjacentHTML("beforeend", markup);
+        });
+    });
+};
+
+// FETCH TRENDING
+
+
 
 // People Fetch
 async function fetchPeople() {
@@ -90,36 +127,7 @@ async function fetchTrending() {
     return data;
 }
 
-const displayTrending = () => {
-    fetchTrending().then((res) => {
-        array1 = res.results;
-        array1.slice(0, 5).forEach((x) => {
-            // console.log(x);
-            const markup = `
-            <div class="markup-container">
-                <div class="overlay-container">
-                    <img class="markup-image" src=${IMAGE_URL + x.poster_path}>
-                    <p class="overlay">${x.overview}</p>
-                </div>
-                <div class="markup-details">
-                    <div class="detail-header">
-                        <h5>${(name = x.original_title
-                            ? `${x.original_title}`
-                            : `${x.original_name}`)}<h5>
-                        <i class="fas fa-crown"> ${x.vote_average}</i>
-                    </div>
-                    <div class="detail-header2">
-                            <h5>${(name = x.release_date ? `${x.release_date}`
-                            : `${x.first_air_date}`)}</h5>
-                            <h5 class="trailer">Trailer<h5>
-                    </div>
-                </div>
-            </div>
-            `;
-            trending.insertAdjacentHTML("beforeend", markup);
-        });
-    });
-};
+
 
 window.onload = (e) => {
     displayPeople();
